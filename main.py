04ea3e881048630027b9945f656c7dd56d514a02
Jamie Wright <jamiechwr97@gmail.com>
2023-05-27 01:10:19 +0200

    Intro texts and conditions added plus null pointer command return fixed

--- main.py
+++ main.py
@@ -4,7 +4,53 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import random
 
+opening_phrases = [
+    {
+        "name": "morning_phrases",
+        "phrases": [
+            "Good morning sir, what can I help you with?",
+            "How may I assist you?",
+            "Good day sir, how can I help you?"
+        ]
+    },
+    {
+        "name": "afternoon_phrases",
+        "phrases": [
+            "Good afternoon sir, what can I help you with?",
+            "How may I assist you?",
+            "Afternoon sir, how can I help you?"
+        ]
+    },
+    {
+        "name": "evening_phrases",
+        "phrases": [
+            "Good evening sir, what can I help you with?",
+            "How may I assist you?",
+            "Good evening sir, how can I help you?"
+        ]
+    },
+    {
+        "name": "night_phrases",
+        "phrases": [
+            "It's very late sir, what can I help you with?",
+            "How may I assist you?",
+            "You're up late sir, how can I help you?"
+        ]
+    },
+]
+repeat_phrases = [
+    "can you repeat please, i didn't understand.",
+    "i didn't hear you properly, can you repeat please?",
+    "can you say that again?"
+]
+various_repeat_phrases = [
+    "excuse me, but I still don't understand. May you please repeat?",
+    "I'm still not to sure what you mean, can you say that again?"
+]
+repeat_question = False
+repeat_times = 0
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -14,12 +60,43 @@ voices = engine.getProperty('voices')
 engine.runAndWait()
 engine.setProperty('voice', voices[1].id)
 
-engine.say('What can I help you with sir?')
-engine.runAndWait()
+def get_opening_phrase(repeat):
+    global repeat_times
+    time = datetime.datetime.now()
+
+    if (time.hour >= 6 and time.minute >= 00) and (time.hour <= 11 and time.minute <= 59) and repeat != True:
+        if repeat_times > 0:
+            repeat_times = 0
+        pos = random.randint(0, len(opening_phrases[0]["phrases"]))
+        talk(opening_phrases[0]["phrases"][pos])
+    elif (time.hour <= 19 and time.minute <= 59) and (time.hour >= 12 and time.minute >= 00) and repeat != True:
+        if repeat_times > 0:
+            repeat_times = 0
+        pos = random.randint(0, len(opening_phrases[0]["phrases"]))
+        talk(opening_phrases[1]["phrases"][pos])
+    elif (time.hour <= 23 and time.minute <= 59) and (time.hour >= 20 and time.minute >= 00) and repeat != True:
+        if repeat_times > 0:
+            repeat_times = 0
+        pos = random.randint(0, len(opening_phrases[0]["phrases"]))
+        talk(opening_phrases[2]["phrases"][pos])
+    elif (time.hour <= 5 and time.minute <= 59) and (time.hour >= 0 and time.minute >= 00) and repeat != True:
+        if repeat_times > 0:
+            repeat_times = 0
+        pos = random.randint(0, len(opening_phrases[0]["phrases"]))
+        talk(opening_phrases[3]["phrases"][pos])
+    elif repeat == True and repeat_times < 1:
+        pos = random.randint(0, 2)
+        talk(repeat_phrases[pos])
+        repeat_times += 1
+    elif repeat == True and repeat_times >= 1:
+        pos = random.randint(0, 1)
+        talk(various_repeat_phrases[pos])
+        repeat_times += 1
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
 def take_command():
     try:
         with sr.Microphone() as source:
@@ -30,30 +107,39 @@ def take_command():
                 command = command.replace('jarvis', '')
                 print(command)
     except:
-        pass
+        command = ''
     return command
-
 def run_jarvis():
+    global repeat_question
+    get_opening_phrase(repeat_question)
     command = take_command()
-    print(command)
+
     if 'play' in command:
         song = command.replace('play', '')
         talk('playing ' + song)
         pywhatkit.playonyt(song)
+        repeat_question = False
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%H:%M')
         print('The current time is ' + time)
         talk('The current time is ' + time)
+        repeat_question = False
     elif 'who is' in command:
     # it will use wikipedia to search for info
         person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
+        repeat_question = False
     elif 'joke' in command:
         talk(pyjokes.get_joke())
+        repeat_question = False
+    elif 'thank you' in command:
+        engine.say('You are welcome.')
+        return False
     else:
-        talk("Please repeat please, i didn't understand.")
+        print('No command found')
+        repeat_question = True
 
 while True:
     run_jarvis()
